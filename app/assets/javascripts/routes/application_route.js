3fdb5f017ffabe83363fcbb65b16b1e7cd5f1cf2
jonathan twaddell <jon@arclocal.com>
2014-05-24 20:45:51 -0700

    updated

--- app/assets/javascripts/routes/application_route.js
+++ app/assets/javascripts/routes/application_route.js
@@ -1,11 +1,11 @@
 Emberjs1.ApplicationRoute = Ember.Route.extend({
     events: {
-        showModal: function() {
+        showModal: function(obj) {
             //Emberjs1.ModalView.create({ title: "My title", content: "My content" }).append();
             //this.createChildView(Emberjs1.ModalView.create({ title: "My title", content: "My content" })).append();
             var v = this.container.lookup('view:modal');
-            v.set('title','My title and other things');
-            v.set('content','more content for you');   
+            v.set('title','hello');
+            v.set('content',obj.name);   
             //var v=Emberjs1.ModalView.create({ title: "My title", content: "My content" });
             v.appendTo(Emberjs1.rootElement);
 
